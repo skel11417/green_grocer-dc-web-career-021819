d2ce47d319ae3a13b87f4ddb26142eeae453c114
Sean Kelly <shonkelli@gmail.com>
2019-02-06 20:20:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,7 +18,7 @@ end
 def apply_coupons(cart, coupons)
   output = {}
   coupons.each do |coupon|
-    if cart.has_key(coupon[:item])
+    if cart.has_key?(coupon[:item])
       output << "fuck, i'm tired"
     end
   end
